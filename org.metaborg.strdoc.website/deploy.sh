f47c76419fbb0ddd180c8da80fcf406244ca917e
Martijn Dwars <ikben@martijndwars.nl>
2018-02-02 13:59:49 +0100

    Bugfix: relative path deployment script

--- org.metaborg.strdoc.website/deploy.sh
+++ org.metaborg.strdoc.website/deploy.sh
@@ -1,5 +1,8 @@
+# Get the relative path to this directory
+MY_PATH="`dirname \"$0\"`"
+
 # Upload everything to the server
 #scp -r * root@146.185.154.205:/var/www/stratego.martijndwars.nl
 
 # Upload only changes to the server
-rsync -avh org.metaborg.strdoc.website/* root@146.185.154.205:/var/www/stratego.martijndwars.nl
+rsync -avh $MY_PATH/* root@146.185.154.205:/var/www/stratego.martijndwars.nl
